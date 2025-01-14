78f95a9d6e450ab63b0eeaea01fd4f6457a8eb7f
aiman-khalid <aimankhalid859@gmail.com>
2025-01-14 12:13:34 +0500

    Mini Calculator

---
+++ Mini Calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=2,b=4;
+	float c=3.67,d=7.89;
+	
+	cout<<"The sum of "<<a<<" and "<<b<<" is "<<a+b<<" . "<<endl;
+	cout<<"The difference of "<<d<<" and "<<b<<" is "<<d-b<<" . "<<endl;
+	cout<<"The product of "<<c<<" and "<<d<<" is "<<c*d<<" . "<<endl;
+	
+	return 0 ;
+	
+}
