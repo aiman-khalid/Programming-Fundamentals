dc9cda874c5b5c9be9f4ec4ef46ac8be3f8e6b33
aiman-khalid <aimankhalid859@gmail.com>
2025-01-16 14:08:10 +0500

    Addition using Functions

---
+++ Function Addition.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string password='secret', input;
+	
+	cout<<"Enter Password:"<<endl;
+	cin>>iput;
+	goto check;
+	
+	check:
+		{
+			if(password==input){
+				cout<<"welcome";
+			}
+			else{
+				attempt++
+			}
+		}
+}
