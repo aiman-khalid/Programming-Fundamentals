306c1e5a299b27d94efe15f27afd37fc8ee64fa3
aiman-khalid <aimankhalid859@gmail.com>
2025-01-17 23:28:07 +0500

    Checks the sign of number

---
+++ Number Sign Detector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int num;
+	
+	cout<<"Enter Any Integer Number:"<<endl;
+	cin>>num;
+	
+	if(num>0){
+		cout<<"The number is positive. "<<endl;
+	}
+	if(num<0){
+		cout<<"The number is negative. "<<endl;
+	}
+	return 0 ;
+}
